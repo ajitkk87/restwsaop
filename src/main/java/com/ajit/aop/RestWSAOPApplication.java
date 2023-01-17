6cbd3866ee7e3ebb101ff25f71aa23ffcf8b99f9
ajitkk87 <invincibleajitkk@gmail.com>
2023-01-17 17:12:30 +0530

    restaop commited

---
+++ src/main/java/com/ajit/aop/RestWSAOPApplication.java
@@ -0,0 +1,13 @@
+package com.ajit.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWSAOPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWSAOPApplication.class, args);
+	}
+
+}
